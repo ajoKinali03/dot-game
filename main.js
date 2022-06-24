5d192aa58721c33c7a695c94eff946dffb9f3cea
kinaliangek1205@gami.com <kinaliangek1205@gami.com>
2022-06-24 21:45:43 +0700

    membuat konsep game pada file js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 // width 325
 // height 620
 
+
+/* konsep game dot:
+---> yaitu dot akan ditekan dan akan membuat dot berhenti apa bila dot berhenti kedua nya maka akan lanjut ke level selanjutnya dan pergerkan dot akan semakin cepat, jika tidak nywa pemain akan berkuran apabila dia tidak mengenai dot tsb dengan bats 4 nyawa atau empat kali percobaan.
+*/
+
+
 const dot = document.getElementsByClassName('dot')[0];
 const dot1 = document.getElementsByClassName('dot')[1];
 setInterval(function(){
