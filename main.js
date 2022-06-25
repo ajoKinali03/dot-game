7a5dbac3bf14bd900e6bc33c50f74db8c46f65f8
kinaliangek1205@gami.com <kinaliangek1205@gami.com>
2022-06-25 17:37:27 +0700

    membuat intro

--- main.js
+++ main.js
@@ -6,24 +6,37 @@
 ---> yaitu dot akan ditekan dan akan membuat dot berhenti apa bila dot berhenti kedua nya maka akan lanjut ke level selanjutnya dan pergerkan dot akan semakin cepat, jika tidak nywa pemain akan berkuran apabila dia tidak mengenai dot tsb dengan bats 4 nyawa atau empat kali percobaan.
 */
 
+/* intro */
+setTimeout(function() {
+  const outro = document.getElementsByClassName('intro')[0];
+  outro.classList.add('outro');
+  setTimeout(function() {
+    outro.style.display = 'none';
+  }, 900);
+}, 5600);
 
-const dot = document.getElementsByClassName('dot')[0];
-const dot1 = document.getElementsByClassName('dot')[1];
-setInterval(function(){
-  dot.style.top = rdmTop()+'px';
-  dot.style.left = rdmLeft()+'px';
-  dot1.style.top = rdmTop()+'px';
-  dot1.style.left = rdmLeft()+'px';
-  dot1.innerHTML = 'you';
-  dot1.style.fontWeight = 'bold';
-}, 500);
 
-function rdmTop(){
-  const nRandom = Math.floor(Math.random() * 620 + 0);
-  return nRandom;
-};
 
-function rdmLeft() {
-  const nRandom = Math.floor(Math.random() * 325 + 0);
-  return nRandom;
-};
+/* start game */
+setTimeout(function(){
+  const dot = document.getElementsByClassName('dot')[0];
+  const dot1 = document.getElementsByClassName('dot')[1];
+  setInterval(function() {
+    dot.style.top = rdmTop() + 'px';
+    dot.style.left = rdmLeft() + 'px';
+    dot1.style.top = rdmTop() + 'px';
+    dot1.style.left = rdmLeft() + 'px';
+    dot1.innerHTML = 'you';
+    dot1.style.fontWeight = 'bold';
+  }, 500);
+  
+  function rdmTop() {
+    const nRandom = Math.floor(Math.random() * 620 + 0);
+    return nRandom;
+  };
+  
+  function rdmLeft() {
+    const nRandom = Math.floor(Math.random() * 325 + 0);
+    return nRandom;
+  };
+}, 6000);
