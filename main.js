8ab3ff575dc1c7fb3807e241fa144c8f9be90302
kinaliangek1205@gami.com <kinaliangek1205@gami.com>
2022-06-27 18:22:27 +0700

    membuat sistem berhenti pada dot

--- main.js
+++ main.js
@@ -39,14 +39,41 @@ plyBtn.addEventListener('click', function(){
       cntrStr.style.display = 'none';
   }, 1000);
   
-  setInterval(() => {
+  const dotClear = setInterval(() => {
     dot.style.top = rdmTop() + 'px';
     dot.style.left = rdmLeft() + 'px';
+  }, 1000);
+  
+  const dotClear1 = setInterval(() => {
     dot1.style.top = rdmTop() + 'px';
     dot1.style.left = rdmLeft() + 'px';
-    dot1.innerHTML = 'you';
-    dot1.style.fontWeight = 'bold';
-  }, 500);
+  }, 1000);
+  
+  
+  let count = 0;
+  dot.addEventListener('click', () => {
+    console.log('ok');
+    clearInterval(dotClear);
+  });
+  
+  dot1.addEventListener('click', () => {
+    console.log('ok');
+    clearInterval(dotClear1);
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
   function rdmTop() {
     const nRandom = Math.floor(Math.random() * (window.innerHeight - 50) + 0);
